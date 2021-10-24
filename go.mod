1b1db79ff08db0080c2f971d0308d1e24c1f6d94
Jeff Prestes <jeffprestes@gmail.com>
2021-10-24 12:20:49 +0100

    working version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jeffprestes/glftexample
+
+go 1.17
+
+require github.com/qmuntal/gltf v0.20.2
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/qmuntal/gltf"
+	"github.com/qmuntal/gltf/modeler"
+)
+
+func main() {
+	doc := gltf.NewDocument()
+	positionAccessor := modeler.WritePosition(doc, [][3]float32{{43, 43, 0}, {83, 43, 0}, {63, 63, 40}, {43, 83, 0}, {83, 83, 0}})
+	indicesAccessor := modeler.WriteIndices(doc, []uint16{0, 1, 2, 3, 1, 0, 0, 2, 3, 1, 4, 2, 4, 3, 2, 4, 1, 3})
+	colorIndices := modeler.WriteColor(doc, [][3]uint8{{50, 155, 255}, {0, 100, 200}, {255, 155, 50}, {155, 155, 155}, {0, 0, 0}})
+	doc.Meshes = []*gltf.Mesh{{
+		Name: "Pyramid",
+		Primitives: []*gltf.Primitive{
+			{
+				Indices: gltf.Index(indicesAccessor),
+				Attributes: map[string]uint32{
+					gltf.POSITION: positionAccessor,
+					gltf.COLOR_0:  colorIndices,
+				},
+			},
+		},
+	}}
+	doc.Nodes = []*gltf.Node{{Name: "Root", Mesh: gltf.Index(0)}}
+	doc.Scenes[0].Nodes = append(doc.Scenes[0].Nodes, 0)
+	if err := gltf.SaveBinary(doc, "./example.glb"); err != nil {
+		panic(err)
+	}
+	fmt.Println("Files saved.")
+}
